a194659affa1f9d0b4cf091dfd51f121fda7624a
Jamie Alquiza <ja@grey-boundary.com>
2014-05-28 19:12:59 -0400

    working clustering

--- langolier.js
+++ langolier.js
@@ -25,141 +25,157 @@
 var settings = require('./settings.js');
 var fs = require('fs');
 var crypto = require('crypto');
+var cluster = require('cluster');
 var elasticsearch = require('elasticsearch');
 var AWS = require('aws-sdk');
 
 
-// --- Init & Misc. --- //
+var workers = settings.workers;
 
-AWS.config.update({
-  accessKeyId: settings.aws.accessKeyId,
-  secretAccessKey: settings.aws.secretAccessKey,
-  region: settings.aws.region
-});
+if (cluster.isMaster) {
+  for (var i = 0; i < workers; i++) {
+    cluster.fork();
+  }
 
-function writeLog(message, level) {
-  if (settings.logConsole) {
-    console.log(Date() + " [" + level + "]: " + message);
-  } else {
-    fs.appendFile(settings.logFile, Date() + " [" + level + "]: " + message + "\n", function (err) {
-      if (err) throw err;
+  cluster.on('exit', function(worker, code, signal) {
+    writeLog("Worker died, respawning", "WARN");
+    cluster.fork();
+  });
+} else {
+
+  // --- Init & Misc. --- //
+
+  AWS.config.update({
+    accessKeyId: settings.sqs.accessKeyId,
+    secretAccessKey: settings.sqs.secretAccessKey,
+    region: settings.sqs.region
+  });
+
+  function writeLog(message, level) {
+    if (settings.logConsole) {
+      console.log(Date() + " [" + level + "]: " + message);
+    } else {
+      fs.appendFile(settings.logFile, Date() + " [" + level + "]: " + message + "\n", function (err) {
+        if (err) throw err;
+      });
+    }
+  }
+
+  function hashId(input) {
+    var hash = crypto.createHash('sha1');
+    hash.setEncoding('hex');
+    hash.write(input);
+    hash.end();
+    return hash.read()
+  }
+
+  // --- Output: ElasticSearch --- //
+
+  // Init
+  var clientEs = new elasticsearch.Client({
+    host: settings.es.host + ':' + settings.es.port,
+    apiVersion: settings.es.apiVer
+  });
+
+  clientEs.ping({
+    requestTimeout: 1000,
+  }, function (err) {
+    if (err) {
+      writeLog(err, "WARN");
+    }
+    else {
+      writeLog("Connected to ElasticSearch on " + settings.es.host + ':' + settings.es.port, "INFO");
+    };
+  });
+
+  // General
+  function indexMsg(message, receipts) {
+    clientEs.bulk({
+      body: message
+    }, function (err, resp) {
+      if (err) {
+        writeLog(err, "WARN");
+      }
+      else {
+        writeLog("Wrote "+ message.length/2 + " items to index '" + settings.es.index + "' in " + resp.took + "ms", "INFO");
+        delSqsMsg(receipts);
+      };
+    });
+  };
+
+
+  // --- Input: AWS SQS --- //
+
+  // Init
+  var sqsParams = {
+    QueueUrl: settings.sqs.sqsUrl,
+    MaxNumberOfMessages: 10,
+    WaitTimeSeconds: 5,
+  };
+
+  function estabSqs() {
+    clientSqs = new AWS.SQS({region: settings.sqs.region});
+    clientSqs.getQueueAttributes({QueueUrl: settings.sqs.sqsUrl}, function(err, data) {
+      if (err) {
+        writeLog(err, "WARN");
+      }
+      else {
+        writeLog("Listening for events on " + settings.sqs.sqsUrl, "INFO");
+      };
     });
   }
-}
 
-function hashId(input) {
-  var hash = crypto.createHash('sha1');
-  hash.setEncoding('hex');
-  hash.write(input);
-  hash.end();
-  return hash.read()
-}
-
-// --- Output: ElasticSearch --- //
-
-// Init
-var clientEs = new elasticsearch.Client({
-  host: settings.es.host + ':' + settings.es.port,
-  apiVersion: settings.es.apiVer
-});
-
-clientEs.ping({
-  requestTimeout: 1000,
-}, function (err) {
-  if (err) {
-    writeLog(err, "WARN");
-  }
-  else {
-    writeLog("Connected to ElasticSearch on " + settings.es.host + ':' + settings.es.port, "INFO");
-  };
-});
-
-// General
-function indexMsg(message, receipts) {
-  clientEs.bulk({
-    body: message
-  }, function (err, resp) {
-    if (err) {
-      writeLog(err, "WARN");
-    }
-    else {
-      writeLog("Wrote "+ message.length/2 + " items to index '" + settings.index + "' in " + resp.took + "ms", "INFO");
-      delSqsMsg(receipts);
-    };
-  });
-};
-
-
-// --- Input: AWS SQS --- //
-
-// Init
-var sqsParams = {
-  QueueUrl: settings.aws.sqsUrl,
-  MaxNumberOfMessages: 10,
-  WaitTimeSeconds: 5,
-};
-
-function estabSqs() {
-  clientSqs = new AWS.SQS({region: settings.aws.region});
-  clientSqs.getQueueAttributes({QueueUrl: settings.aws.sqsUrl}, function(err, data) {
-    if (err) {
-      writeLog(err, "WARN");
-    }
-    else {
-      writeLog("Listening for events on " + settings.aws.sqsUrl, "INFO");
-    };
-  });
-}
-
-// General
-function parseSqsMsg(messages) {
-  var msgCount = messages.length;
-  var docs = [];
-  var receipts = [];
-  var meta = {};
-  var doc = {};
-  var receipt = {};
-  for (var msg = 0; msg < msgCount; msg++) {
-    var rcpt = messages[msg].ReceiptHandle;
-    var body = JSON.parse(messages[msg].Body);
-    meta = { index: {
-        _index: settings.index,
+  // General
+  function parseSqsMsg(messages) {
+    var msgCount = messages.length;
+    var docs = [];
+    var receipts = [];
+    var meta = {};
+    var doc = {};
+    var receipt = {};
+    for (var msg = 0; msg < msgCount; msg++) {
+      var rcpt = messages[msg].ReceiptHandle;
+      var body = JSON.parse(messages[msg].Body);
+      meta = { index: {
+        _index: settings.es.index,
         _type: body.DataType,
         _id: hashId(JSON.stringify(body.Message)) }
       };
-    doc = body.Message;
-    receipt = { Id: msg.toString(), ReceiptHandle: rcpt };
-    docs.push(meta, doc);
-    receipts.push(receipt);
-  };
-  //console.log(docs)
-  indexMsg(docs, receipts)
-}
-
-function pollSqs() {
-  clientSqs.receiveMessage(sqsParams, function (err, data) {
-    if (err) {
-      writeLog(err, "WARN");
-    } else {
-      if (data.Messages) {
-        parseSqsMsg(data.Messages)
-      };
-    pollSqs(); // Long-poll for 'WaitTimeSeconds', immediate call to self if message is available
+      doc = body.Message;
+      receipt = { Id: msg.toString(), ReceiptHandle: rcpt };
+      docs.push(meta, doc);
+      receipts.push(receipt);
     };
-  });
-};
+    //console.log(docs)
+    indexMsg(docs, receipts)
+  }
 
-// Callback on successful ES indexing
-function delSqsMsg(receipts) {
-  clientSqs.deleteMessageBatch({
-    QueueUrl: settings.aws.sqsUrl,
-    Entries: receipts
-  }, function(err, resp) {
-    if (err) writeLog(err, "WARN")
-  });
-};
+  function pollSqs() {
+    clientSqs.receiveMessage(sqsParams, function (err, data) {
+      if (err) {
+        writeLog(err, "WARN");
+      } else {
+        if (data.Messages) {
+          parseSqsMsg(data.Messages)
+        };
+        pollSqs(); // Long-poll for 'WaitTimeSeconds', immediate call to self if message is available
+      };
+    });
+  };
 
-// Service
-estabSqs(); // Initial connection
-setInterval(estabSqs, 300000); // Refresh conn; AWS API has 15 min timeout
-pollSqs();
+  // Callback on successful ES indexing
+  function delSqsMsg(receipts) {
+    clientSqs.deleteMessageBatch({
+      QueueUrl: settings.sqs.sqsUrl,
+      Entries: receipts
+    }, function(err, resp) {
+      if (err) writeLog(err, "WARN")
+    });
+  };
+
+  // Service
+  estabSqs(); // Initial connection
+  setInterval(estabSqs, 300000); // Refresh conn; AWS API has 15 min timeout
+  pollSqs();
+
+}
--- settings.js
+++ settings.js
@@ -1,21 +1,22 @@
 var settings = {};
 settings.redis = {};
 settings.es = {};
-settings.aws = {};
+settings.sqs = {};
 
 // General
-settings.index = 'index';
+settings.workers = 4;
 settings.logFile = '/var/log/langolier.log';
 // If logConsole is true, logFile will not be written.
 settings.logConsole = false;
 
 // Input: Amazon SQS
-settings.aws.accessKeyId = '';
-settings.aws.secretAccessKey = '';
-settings.aws.region = '';
-settings.aws.sqsUrl = '';
+settings.sqs.accessKeyId = '';
+settings.sqs.secretAccessKey = '';
+settings.sqs.region = '';
+settings.sqs.sqsUrl = '';
 
 // Output: ElasticSearch
+settings.es.index = 'index';
 settings.es.host = '127.0.0.1';
 settings.es.port = '9200';
 settings.es.apiVer = '1.1';
